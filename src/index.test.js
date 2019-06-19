307cc59d403d81681553a7cb460d465adfa5010c
ifeanyi <aifeanyi019@gmail.com>
2019-06-19 14:33:32 +0100

    fixed the broken build issue

--- src/index.test.js
+++ src/index.test.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 
 describe('Our first test', () => {
   it('should pass', () => {
-    expect(true).to.equal(false);
+    expect(true).to.equal(true);
   });
 });
 
@@ -13,7 +13,7 @@ describe('index.html', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello World?");
+      expect(h1.innerHTML).to.equal("Hello World!");
       done();
       window.close();
     })
